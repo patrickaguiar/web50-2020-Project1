e570f6c0b61fe9786d66aae5a6275339420aa62b
Patrick Aguiar <patrickaguiar859@gmail.com>
2020-07-06 12:01:16 -0300

    create index page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,10 +5,11 @@ from . import views
 app_name='encyclopedia'
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name='index'),
+    path("wiki/", views.wiki, name="wiki"),
     path("<str:entry_name>", views.entry, name='entry'),
     path("search/", views.search, name='search'),
     path("create_new_page/", views.create_new_page, name='create_new_page'),
     path("edit_entry/", views.edit_entry, name='edit_entry'),
-    path("random_entry/", views.random_entry, name='random_entry')
+    path("random_entry/", views.random_entry, name='random_entry'),
 ]
